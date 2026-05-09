70211ce96c44d21d459f687388215f4bc6a34899
Soumya1709 <soumyaranchi05@gmail.com>
2026-05-09 17:57:29 +0530

    Updated frontend API URL

--- frontend/src/api/resumeApi.js
+++ frontend/src/api/resumeApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://rank-my-cv.onrender.com/api",
 });
 
 export const uploadResume = async (resumeFile, jobDescription) => {
